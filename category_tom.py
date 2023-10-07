9b0984444a2194073241414f065a3977ee833439
tomestes310 <147217259+tomestes310@users.noreply.github.com>
2023-10-06 17:47:09 -0700

    Update category_tom.py

--- category_tom.py
+++ category_tom.py
@@ -1,10 +1,19 @@
 default_categories = {
    "Website Design & Navigation": {
-       "Issue1": [
-            "Facing issues related to issue1.",
-            "Issue1 wasn't as expected.",
-            "Problems encountered with issue1."
-        ],
+    "Accessibility": ["Need better accessibility options.", "Improve site for disabilities.", "Enhance screen reader support."],
+    "Account Security": ["Strengthen my account security.", "Secure my login details.", "Enable two-factor authentication."],
+    "App View": ["App layout needs improvement.", "App not user-friendly.", "Mobile app crashes frequently."],
+    "App View EPP": ["EPP app view is slow.", "Enterprise app view issues.", "Enhance business app layout."],
+    "Applications": ["Need better software options.", "Recommend useful mobile apps.", "Explore web-based applications."],
+    "Comparison": ["Compare product features.", "Which is better? "Show me alternatives."],
+    "Ease of Use": ["Make it more user-friendly.", "Streamline the interface.", "Difficult to navigate."],
+    "Pop Ups": ["Stop annoying pop-ups.", "Block intrusive notifications.", "Manage ad pop-ups."],
+    "Promotion Availability": ["Check available promotions.", "Are deals still active? "Any current discounts?],
+    "Promotional Offers": ["Show available discounts.", "Any special promotions? "List ongoing deals."],
+    "Scrolling": ["Improve page scrolling.", "Scrolling is jumpy.", "Fix scrolling issues."],
+    "Sort Feature": ["Can't sort products.", "Sort options missing.", "Custom sort not working."],
+    "Unable to Find Product": ["Can't locate item.", "Product not in stock.", "Can't find what's needed."],
+    "Website Search Feature": ["Improve search functionality.", "Search isn't accurate.", "How to use search?],
     },
    "Product Information": {
        "Issue1": [
@@ -25,7 +34,7 @@ default_categories = {
    "Damages" : ["product arrived damaged", "broken item on delivery", "damaged during shipping"],
    "Empty Box" : ["box was empty", "received an empty package", "no product inside the box"],
    "Incorrect Delivery Dates - Post" : ["delivery date changed", "was supposed to get it earlier", "wrong delivery estimate"],
-   "Installation Issues" : ["problem with installation", "the installer didn't set it up", "installation service was poor"],
+   "Installation Issues" : ["problem with installation", "can't set it up", "installation service was poor"],
    "Lack of Delivery Delay Notification" : ["wasn't informed about the delay", "no notification for late delivery", "should have told me it was delayed"],
    "Lack of Delivery Service" : ["no delivery service offered", "can't get it delivered", "no shipping options available"],
    "Lack of Delivery Service AGS" : ["AGS didn't deliver", "no AGS delivery service", "why isn't AGS delivering?"],
